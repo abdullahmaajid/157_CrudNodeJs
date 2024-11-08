e68a4c22e1837d01218f034de90bfae1f48cab99
abdullahmaajid <abdullah.maajid.ft22@mail.umy.ac.id>
2024-11-08 13:54:09 +0700

    dbcrud_157

--- app.js
+++ app.js
@@ -1,22 +1,19 @@
 const express = require('express');
-const todoRoutes = require('./Routes/todo.js');
 const app = express();
-const port = 3000;
+const todoRoutes = require('./Routes/tododb.js');
+require('dotenv').config();
+const port = process.env.PORT;
 
 app.use(express.json());
 
 app.use('/todos', todoRoutes);
+
 app.set('view engine', 'ejs');
 app.get('/', (req, res) => {
     res.render('index');
 });
-
 app.get('/contact', (req, res) => {
     res.render('contact');
-});
-
-app.use((req, res) => {
-    res.status(404).send('404 - Page Not Found');
 });
 
 app.listen(port, () => {
